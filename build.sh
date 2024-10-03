b335f1e5c8021619a2408ba312739ca6c6580146
Cody Mullins <joshua.c.mullins@gmail.com>
2024-10-02 21:27:16 -0400

    add build.sh

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
+chmod +x dotnet-install.sh
+./dotnet-install.sh -c 8.0 -InstallDir ./dotnet
+./dotnet/dotnet --version
+./dotnet/dotnet publish -c Release -o output
